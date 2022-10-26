afc3e662122948b5956dadd9956545c00afb170f
bhendi-boi <bhendiisgreen@gmail.com>
2022-10-26 10:35:16 +0530

    fortigate update

--- fortigate.py
+++ fortigate.py
@@ -4,14 +4,30 @@ import time
 # run this script and open your browser within 5 min to for it to work properly
 username: str = ""
 password: str = ""
+browserNames = ["brave", "google chrome"]
 
 data = {"userName": username, "password": password}
-time.sleep(5)
-pyautogui.leftClick(x=500, y=600)
-pyautogui.write("\t")
-time.sleep(2)
-pyautogui.write(data["userName"] + "\t")
-time.sleep(2)
-pyautogui.write(data["password"])
-time.sleep(2)
-pyautogui.press("enter")
+
+
+def core() -> bool:
+    pyautogui.leftClick(x=500, y=600)
+    pyautogui.write("\t")
+    time.sleep(2)
+    pyautogui.write(data["userName"] + "\t")
+    time.sleep(2)
+    pyautogui.write(data["password"])
+    time.sleep(2)
+    pyautogui.press("enter")
+    return True
+
+
+while True:
+    fw = pyautogui.getActiveWindow()
+    tabName = fw.title.lower()
+    flag = False
+    for browser in browserNames:
+        if browser in tabName:
+            flag = core()
+    if flag:
+        break
+    time.sleep(2)
